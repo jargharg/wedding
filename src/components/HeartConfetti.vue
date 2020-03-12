502faf03bbce17f342df5b2818ef62d74b5a1284
Jarod Hargreaves <jarod.hargreaves@gmail.com>
2020-03-12 12:21:43 +0000

    Refactor hearts animation

--- src/components/HeartConfetti.vue
+++ src/components/HeartConfetti.vue
@@ -5,11 +5,12 @@
 			:key="index"
 			class="heart"
 			xmlns="http://www.w3.org/2000/svg"
-			viewBox="0 0 24 24"
+			viewBox="-1 -1 26 26"
 		>
 			<path
 				stroke="rgb(0, 57, 63)"
 				fill="rgb(255, 198, 192)"
+				vector-effect="non-scaling-stroke"
 				d="M12 4.248c-3.148-5.402-12-3.825-12 2.944 0 4.661 5.571 9.427 12 15.808 6.43-6.381 12-11.147 12-15.808 0-6.792-8.875-8.306-12-2.944z"
 			/>
 		</svg>
@@ -29,18 +30,18 @@ export default {
 	methods: {
 		sprayConfetti(event) {
 			const { top, left, height, width } = event.target.getBoundingClientRect();
-			const heartsBoxLocation = this.$refs.hearts.getBoundingClientRect();
+			const containerLocation = this.$refs.hearts.getBoundingClientRect();
 
-			const startX = left + height / 2 - heartsBoxLocation.left;
-			const startY = top + width / 2 - heartsBoxLocation.top;
+			const startX = left + height / 2 - containerLocation.left;
+			const startY = top + width / 2 - containerLocation.top;
 
-			const heartsContainer = document.querySelector('.hearts');
+			const container = document.querySelector('.hearts');
 			const hearts = Array.from(document.querySelectorAll('.heart'));
 
-			gsap.set(heartsContainer, { visibility: 'visible' });
+			gsap.set(container, { visibility: 'visible' });
 			gsap.set(hearts, { scale: 0 });
 
-			const heartPop = () => {
+			const playAnimation = () => {
 				if (this.currentAnimation) this.currentAnimation.kill();
 
 				this.currentAnimation = gsap
@@ -54,31 +55,41 @@ export default {
 					});
 
 				hearts.forEach((heart, index) => {
-					const heartAnimation = gsap
-						.timeline()
-						.to(heart, {
-							delay: index * 0.02,
-							duration: 0.3,
-							ease: 'power1.out',
-							rotate: 60 - Math.random() * 120,
-							scale: 0.2 + Math.random() * 1,
-							x: startX + 150 - Math.random() * 300,
-							y: startY - 50 - Math.random() * 150,
-						})
-						.to(heart, {
-							duration: 1,
-							ease: 'power1.in',
-							rotate: 0,
-							scale: 0,
-							y: startY + 200,
-						})
-						.set(heart, { x: 0, y: 0 });
+					const heartAnimation = this.generateHeartAnimation({
+						heart,
+						index,
+						startX,
+						startY,
+					});
 
 					this.currentAnimation.add(heartAnimation, 'start');
 				});
 			};
 
-			heartPop();
+			playAnimation();
+		},
+		generateHeartAnimation({ heart, index, startX, startY }) {
+			const randomX = Math.random();
+
+			return gsap
+				.timeline()
+				.to(heart, {
+					delay: index * 0.02,
+					duration: 0.3,
+					ease: 'power3.out',
+					rotate: 60 - randomX * 120,
+					scale: 0.2 + Math.random(),
+					x: startX + 150 - randomX * 300,
+					y: startY - 50 - Math.random() * 150,
+				})
+				.to(heart, {
+					duration: 1,
+					ease: 'power1.in',
+					rotate: 0,
+					scale: 0,
+					y: startY + 200,
+				})
+				.set(heart, { x: 0, y: 0 });
 		},
 	},
 };
@@ -97,6 +108,6 @@ export default {
 	pointer-events: none;
 	z-index: 100;
 	position: absolute;
-	opacity: 0.9;
+	opacity: 0.95;
 }
 </style>
